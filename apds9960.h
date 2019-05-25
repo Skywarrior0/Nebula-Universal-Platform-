dc22b62f26d60ca7d087f8e89b81c3b6797b99e1
Skywarrior0 <mergonpierre0@gmail.com>
2019-05-25 17:36:00 -0400

    Update apds9960.h

--- apds9960.h
+++ apds9960.h
@@ -5,4 +5,22 @@
 
 #define proxEnable 0x8
 
+//Enable Register 0x80
+//reserved 7
+#define GEN 6
+#define PIEN 5
+#define AIEN 4
+#define WEN 3
+#define PEN 2
+#define AEN 1
+#define  PON 0
+//WAIT TIME REGISTER 0x83
+#define WTIME[7]
+//PROXIMITY INTERRUPT THRESTHOLD
+#define PILT 0x89
+#define PIHT 0x8B
+// PERSISTENCE REGISTER 0x8C
+#define PPERS 
+
+
 #endif 
